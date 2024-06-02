a3945e6e0a0e0713d261e3689076b0c4c6a3c7cf
anshugpt <anshugupta@gmail.com>
2024-06-02 12:31:19 +0530

    removed useless imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { useDispatch } from 'react-redux'
 import authService from './appwrite/auth'
